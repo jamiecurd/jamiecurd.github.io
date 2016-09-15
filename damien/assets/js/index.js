7c51dba1d3bcf22477f3565ec12b00775dfb577c
Jamie Curd <jamie.curd@gmail.com>
2016-09-15 21:00:51 +1200

    Update

--- damien/assets/js/index.js
+++ damien/assets/js/index.js
@@ -22,4 +22,21 @@ jQuery(document).ready(function($){
 		$('h1').css('color','#EEEEEE');
 	}, 3000);
 
+	var isLateralNavAnimating = false;
+
+//open/close lateral navigation
+$('.cd-nav-trigger').on('click', function(event){
+	event.preventDefault();
+	//stop if nav animation is running
+	if( !isLateralNavAnimating ) {
+		if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true;
+
+		$('body').toggleClass('navigation-is-open');
+		$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			//animation is over
+			isLateralNavAnimating = false;
+		});
+	}
+});
+
 });
